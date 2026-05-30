19d3ccb07bb398d13129f86e62868d75b91ea21d
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-05-31 02:27:10 +0300

    attrib: Utilize getters for TABLE's 'visibility' field in x_window_add_items()

--- libleptonattrib/src/x_window.c
+++ libleptonattrib/src/x_window.c
@@ -824,7 +824,7 @@ x_window_add_items()
       {
         /* NULL = no entry */
         text = g_strdup (attrib_table_get_attrib_value (component_table, i, j));
-        visibility = (component_table)[i][j].visibility;
+        visibility = attrib_table_get_visibility (component_table, i, j);
         show_name_value = (component_table)[i][j].show_name_value;
         x_gtksheet_add_cell_item (GTK_SHEET (attrib_get_sheet (0)), i, j, (gchar *) text,
                                   visibility, show_name_value );
@@ -842,7 +842,7 @@ x_window_add_items()
       {
         /* NULL = no entry */
         text = g_strdup (attrib_table_get_attrib_value (net_table, i, j));
-        visibility = (net_table)[i][j].visibility;
+        visibility = attrib_table_get_visibility (net_table, i, j);
         show_name_value = (net_table)[i][j].show_name_value;
         x_gtksheet_add_cell_item (GTK_SHEET (attrib_get_sheet (1)), i, j, (gchar *) text,
                                   visibility, show_name_value );
