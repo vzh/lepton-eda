cf4776b5ba47d751d07237ab21c0418dd01d67de
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-05-31 07:51:38 +0300

    attrib: Obtain cell text once

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -472,15 +472,14 @@ Please check your design.")))
    (lambda (i)
      (for-each
       (lambda (j)
-        (unless (null-pointer?
-                 (attrib_table_get_attrib_value *component-table i j))
-          ;; NULL = no entry.
-          (let ((*text
-                 (attrib_table_get_attrib_value *component-table i j))
-                (visibility
-                 (attrib_table_get_visibility *component-table i j))
-                (show-name-value
-                 (attrib_table_get_show_name_value *component-table i j)))
+        (let ((*text
+               (attrib_table_get_attrib_value *component-table i j))
+              (visibility
+               (attrib_table_get_visibility *component-table i j))
+              (show-name-value
+               (attrib_table_get_show_name_value *component-table i j)))
+          (unless (null-pointer? *text)
+            ;; NULL = no entry.
             (x_gtksheet_add_cell_item (component-sheet)
                                       i
                                       j
@@ -495,14 +494,13 @@ Please check your design.")))
    (lambda (i)
      (for-each
       (lambda (j)
-        (unless (null-pointer?
-                 (attrib_table_get_attrib_value *net-table i j))
-          ;; NULL = no entry.
-          (let ((*text
-                 (attrib_table_get_attrib_value *net-table i j))
-                (visibility (attrib_table_get_visibility *net-table i j))
-                (show-name-value
-                 (attrib_table_get_show_name_value *net-table i j)))
+        (let ((*text (attrib_table_get_attrib_value *net-table i j))
+              (visibility
+               (attrib_table_get_visibility *net-table i j))
+              (show-name-value
+               (attrib_table_get_show_name_value *net-table i j)))
+          (unless (null-pointer? *text)
+            ;; NULL = no entry.
             (x_gtksheet_add_cell_item (net-sheet)
                                       i
                                       j
@@ -517,13 +515,11 @@ Please check your design.")))
    (lambda (i)
      (for-each
       (lambda (j)
-        (unless (null-pointer?
-                 (attrib_table_get_attrib_value *pin-table i j))
-          ;; NULL = no entry.
-          (let ((*text
-                 (attrib_table_get_attrib_value *pin-table i j)))
-            ;; Pins have no visibility attributes, must
-            ;; therefore provide default.
+        (let ((*text (attrib_table_get_attrib_value *pin-table i j)))
+          ;; Pins have no visibility attributes, must
+          ;; therefore provide default.
+          (unless (null-pointer? *text)
+            ;; NULL = no entry.
             (x_gtksheet_add_cell_item (pin-sheet)
                                       i
                                       j
