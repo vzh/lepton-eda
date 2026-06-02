e3ce03e5db028d0145be2043afbd80cf8ff29ddd
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-02 12:16:22 +0300

    attrib: Enable processing of nets in x_gtksheet_init()

--- libleptonattrib/src/x_gtksheet.c
+++ libleptonattrib/src/x_gtksheet.c
@@ -137,7 +137,6 @@ x_gtksheet_init()
   }
 
 
-#ifdef UNIMPLEMENTED_FEATURES
   /* -----  Nets  ----- */
   if ((sheet_head->net_count > 0) && (sheet_head->net_attrib_count >0)) {
     sheets[1] = (GtkSheet *) gtk_sheet_new(sheet_head->net_count, sheet_head->net_attrib_count, _("Nets"));
@@ -150,7 +149,6 @@ x_gtksheet_init()
     gtk_sheet_column_button_justify(sheets[1], 0, GTK_JUSTIFY_LEFT);
     gtk_sheet_set_locked(GTK_SHEET(sheets[1]), TRUE);   /* disallow editing of attribs for now */
   }
-#endif
 
 
   /* -----  Pins  ----- */
