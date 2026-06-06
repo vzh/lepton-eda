629677386fbc7b17e69fd953f78838080313c16a
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-06 18:24:25 +0300

    attrib: Utilize getter for sheet data component table in s_toplevel_add_new_attrib()

--- libleptonattrib/src/s_toplevel.c
+++ libleptonattrib/src/s_toplevel.c
@@ -95,9 +95,10 @@ void s_toplevel_add_new_attrib(gchar *new_attrib_name) {
 
     /* resize table to accomodate new attrib col */
     sheet_head->component_table =
-      s_table_resize(sheet_head->component_table,
-                     sheet_head->comp_count,
-                     old_comp_attrib_count, sheet_head->comp_attrib_count);
+      s_table_resize (attrib_sheet_data_get_component_table (sheet_head),
+                      sheet_head->comp_count,
+                      old_comp_attrib_count,
+                      sheet_head->comp_attrib_count);
 
     g_debug ("s_toplevel_add_new_attrib: Resized component table.\n");
 
