2f02711f780b0b553a8b7e305dad5b821bca90e5
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-08 04:27:44 +0300

    attrib: Utilize getter for component table cell property 'show_name_value'

--- libleptonattrib/src/s_toplevel.c
+++ libleptonattrib/src/s_toplevel.c
@@ -152,7 +152,8 @@ s_toplevel_update_component_attribs_in_toplevel (LeptonToplevel *toplevel,
   } else { /* we need a better place to get this info since the TABLE can be out of date */
     visibility =
       attrib_table_get_visibility (attrib_sheet_data_get_component_table (sheet_head), row, col);
-    show_name_value = attrib_sheet_data_get_component_table (sheet_head)[row][col].show_name_value;
+    show_name_value =
+      attrib_table_get_show_name_value (attrib_sheet_data_get_component_table (sheet_head), row, col);
   }
   g_free(refdes);
 
