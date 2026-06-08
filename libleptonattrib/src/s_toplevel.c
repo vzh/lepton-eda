9cf1af16296157f047ca557f13f8b7188d4a01dd
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-08 04:19:00 +0300

    attrib: Utilize getter for component list in s_toplevel_update_component_attribs_in_toplevel()

--- libleptonattrib/src/s_toplevel.c
+++ libleptonattrib/src/s_toplevel.c
@@ -143,7 +143,7 @@ s_toplevel_update_component_attribs_in_toplevel (LeptonToplevel *toplevel,
    * visibility of the new attrib stored in the component table */
   /* We'll need this later */
   refdes = g_strdup(s_attrib_get_refdes(o_current));
-  row = s_table_get_index(sheet_head->master_comp_list_head, refdes);
+  row = s_table_get_index (attrib_sheet_data_get_component_list (sheet_head), refdes);
   col = s_table_get_index(sheet_head->master_comp_attrib_list_head, new_attrib_name);
   /* if attribute has been deleted from the sheet, here is where we detect that */
   if ( (row == -1) || (col == -1) ) {
