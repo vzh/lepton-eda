b259efd62bbc9837f254f350eff3ceda4a555206
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-12 00:58:20 +0300

    attrib: Add getter for pin attrib counter address

--- libleptonattrib/include/prototype.h
+++ libleptonattrib/include/prototype.h
@@ -147,6 +147,9 @@ attrib_sheet_data_get_pin_attrib_count (SHEET_DATA *data);
 void
 attrib_sheet_data_set_pin_attrib_count (SHEET_DATA *data,
                                         int count);
+int*
+attrib_sheet_data_get_pin_attrib_counter_address (SHEET_DATA *data);
+
 int
 attrib_sheet_data_get_changed (const SHEET_DATA* data);
 
--- libleptonattrib/src/s_sheet_data.c
+++ libleptonattrib/src/s_sheet_data.c
@@ -608,6 +608,22 @@ attrib_sheet_data_set_pin_attrib_count (SHEET_DATA *data,
 }
 
 
+/*! \brief Get the address of pin attrib counter of sheet data.
+ *
+ *  \par Function Description
+ *
+ *  Returns the address of pin attrib counter of given sheet data
+ *  structure.
+ *
+ *  \return The address.
+ */
+int*
+attrib_sheet_data_get_pin_attrib_counter_address (SHEET_DATA *data)
+{
+  return &(data->pin_attrib_count);
+}
+
+
 /*! \brief Get the \a changed flag of sheet data.
  *
  *  \par Function Description
