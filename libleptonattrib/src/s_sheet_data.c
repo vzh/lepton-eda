4d7b90813577fd8fe026f7f0ddb4aafed9507c2b
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-12 00:43:58 +0300

    attrib: Utilize getter for component attrib list in s_sheet_data_add_master_comp_attrib_list_items()

--- libleptonattrib/src/s_sheet_data.c
+++ libleptonattrib/src/s_sheet_data.c
@@ -802,7 +802,7 @@ void s_sheet_data_add_master_comp_attrib_list_items (const GList *obj_list) {
               g_debug ("... from this component, "
                        "about to add to master comp attrib list attrib=%s\n",
                        attrib_name);
-              s_string_list_add_item(sheet_head->master_comp_attrib_list_head,
+              s_string_list_add_item (attrib_sheet_data_get_component_attrib_list (sheet_head),
                                      &(sheet_head->comp_attrib_count), attrib_name);
             }   /* if (strcmp(attrib_name, "refdes") != 0) */
             g_free(attrib_name);
