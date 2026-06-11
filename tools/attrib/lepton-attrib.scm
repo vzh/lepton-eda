3148b5a3f7bd4ee80604e6fb2e46d104c4e68f69
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-12 00:18:35 +0300

    attrib: Add Scheme mate for s_sheet_data_add_master_net_list_items()

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -792,6 +792,10 @@ Please check your design.")))
   (s_sheet_data_add_master_comp_attrib_list_items *objects))
 
 
+(define (add-nets *objects)
+  (s_sheet_data_add_master_net_list_items *objects))
+
+
 (define (activate *app *toplevel)
   (define *window-widget (attrib_window_new *app))
   (define *sheet-data (make-sheet-data))
@@ -818,8 +822,7 @@ Please check your design.")))
      ;; Note that this must be changed.  We need to input the
      ;; entire project before doing anything with the nets because
      ;; we need to first determine where they are all connected!
-     (s_sheet_data_add_master_net_list_items
-      (lepton_page_objects *page))
+     (add-nets (lepton_page_objects *page))
      (s_sheet_data_add_master_net_attrib_list_items
       (lepton_page_objects *page))
 
