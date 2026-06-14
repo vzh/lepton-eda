46e36c5e5120a0dee995615b045467da22ec2ae3
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-14 14:34:52 +0300

    attrib: Utilize getter for pin list in s_table_add_toplevel_pin_items_to_pin_table()

--- libleptonattrib/src/s_table.c
+++ libleptonattrib/src/s_table.c
@@ -678,7 +678,7 @@ void s_table_add_toplevel_pin_items_to_pin_table (const GList *obj_list) {
                    */
 
                   /* Get row and col where to put this attrib */
-                  row = s_table_get_index(sheet_head->master_pin_list_head, row_label);
+                  row = s_table_get_index (attrib_sheet_data_get_pin_list (sheet_head), row_label);
                   col = s_table_get_index(sheet_head->master_pin_attrib_list_head, attrib_name);
                   /* Sanity check */
                   if (row == -1 || col == -1) {
