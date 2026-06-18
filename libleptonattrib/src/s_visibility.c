7ed7a2efa39456175b2f3deb7f841d900a46a56a
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-18 18:08:43 +0300

    attrib: Avoid program crashes when no cell is selected
    This can happen, for example, on program startup, when no cell is
    selected and the user changes visibility from menu.

--- libleptonattrib/src/s_visibility.c
+++ libleptonattrib/src/s_visibility.c
@@ -359,14 +359,17 @@ void s_visibility_set_cell(gint cur_page, gint row, gint col,
     break;
   }
 
-  /* Question:  how to sanity check (row, col) selection? */
-  local_table[row][col].visibility = visibility;
-  /* cell has been updated.  */
-  s_sheet_data_set_changed (sheet_head, TRUE);
-
-  if (show_name_value != LEAVE_NAME_VALUE_ALONE) {
-    local_table[row][col].show_name_value = show_name_value;
+  if ((row != -1) && ( col != -1))
+  {
+    /* Question:  how to sanity check (row, col) selection? */
+    local_table[row][col].visibility = visibility;
     /* cell has been updated.  */
     s_sheet_data_set_changed (sheet_head, TRUE);
+
+    if (show_name_value != LEAVE_NAME_VALUE_ALONE) {
+      local_table[row][col].show_name_value = show_name_value;
+      /* cell has been updated.  */
+      s_sheet_data_set_changed (sheet_head, TRUE);
+    }
   }
 }
