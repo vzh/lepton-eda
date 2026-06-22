2b7a9ce945b46020029e6738c8243c5a26b32b8c
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-22 12:57:31 +0300

    attrib: Create main program window box in Scheme

--- lepton-eda/scheme/attrib/ffi.scm
+++ lepton-eda/scheme/attrib/ffi.scm
@@ -289,4 +289,4 @@
 (define-lff attrib_window_new '* '(*))
 (define-lff attrib_window_sheets_new void '())
 (define-lff attrib_window_set_menu_callback void '(* *))
-(define-lff x_window_init void '())
+(define-lff x_window_init void '(*))
