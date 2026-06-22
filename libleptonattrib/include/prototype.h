c3ff5ba39f7b59ad1c1efb6e7cde97eaa755abbc
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-22 13:13:38 +0300

    attrib: Factor out a function creating the menubar widget

--- libleptonattrib/include/prototype.h
+++ libleptonattrib/include/prototype.h
@@ -287,6 +287,9 @@ int
 attrib_run (gpointer activate_callback,
             LeptonToplevel *toplevel);
 GtkWidget*
+attrib_window_menubar_new (GtkWidget *window);
+
+GtkWidget*
 attrib_window_new (gpointer app);
 
 void
