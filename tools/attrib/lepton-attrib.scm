ed67004bf666d5d4f00edcdb8f07871ba3071839
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-22 12:23:00 +0300

    attrib: Add Scheme mate for x_window_init()

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -1510,6 +1510,10 @@ Choose \"Quit\" to leave lepton-attrib and fix the problem, or
   (attrib_window_set_menu_callback (string->pointer "help-about")
                                    *callback-help-about))
 
+(define (init-window-widgets)
+  (x_window_init))
+
+
 (define (init-window)
   (define *window-widget (attrib_get_window))
 
@@ -1521,7 +1525,7 @@ Choose \"Quit\" to leave lepton-attrib and fix the problem, or
   ;; Init menu functions.
   (init-callbacks)
 
-  (x_window_init)
+  (init-window-widgets)
 
   ;; Create the array of sheets.
   (attrib_window_sheets_new)
