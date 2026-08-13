7a45d156f9df1ce9c1bb44c7ff8ecce5f9541530
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-08-13 05:53:50 +0300

    attrib: Rewrite x_dialog_confirm_overwrite() in Scheme

--- lepton-eda/scheme/attrib/ffi.scm
+++ lepton-eda/scheme/attrib/ffi.scm
@@ -53,7 +53,6 @@
 
             x_fileselect_open
 
-            x_dialog_confirm_overwrite
             x_dialog_fatal_error
             x_dialog_unimplemented_feature
 
@@ -201,7 +200,6 @@
 (define-lff x_fileselect_open '* '())
 
 ;;; x_dialog.c
-(define-lff x_dialog_confirm_overwrite int '(*))
 (define-lff x_dialog_fatal_error void (list '* int))
 (define-lff x_dialog_unimplemented_feature void '())
 
