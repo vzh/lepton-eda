26e9b253560bdf2e0bb460ed697b3d7d2095482e
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-09-10 14:27:19 +0300

    schematic: Add FFI definition for gtk_message_dialog_set_markup()

--- lepton-eda/scheme/schematic/ffi/gtk.scm
+++ lepton-eda/scheme/schematic/ffi/gtk.scm
@@ -100,6 +100,7 @@
             gtk_menu_shell_append
 
             gtk_message_dialog_new
+            gtk_message_dialog_set_markup
 
             gtk_notebook_append_page
             gtk_notebook_get_n_pages
@@ -267,6 +268,7 @@
 (define-lff gtk_menu_shell_append void '(* *))
 
 (define-lff gtk_message_dialog_new '* (list '* int int int '*))
+(define-lff gtk_message_dialog_set_markup void '(* *))
 
 (define-lff gtk_notebook_append_page int '(* * *))
 (define-lff gtk_notebook_get_n_pages int '(*))
